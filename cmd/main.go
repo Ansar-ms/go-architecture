063c6935dc89a8be4b6b4b3762c95546d14062e2
ansarms777 <ansar.tblr@gmail.com>
2024-06-27 18:01:10 +0530

    swagger added

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,32 @@
 package main
 
 import (
+	_ "go-architecture/cmd/docs"
 	"go-architecture/pkg/config"
 	"go-architecture/pkg/server"
 	"log"
 )
 
+// @title User Management API
+// @version 1.0
+// @description This is a sample server for managing users.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
+
+// @securityDefinitions.apiKey JWT
+// @in header
+// @name Authorization
+
+// @schemes http https
 func main() {
 	// Load configuration
 	cfg, err := config.LoadConfig()
